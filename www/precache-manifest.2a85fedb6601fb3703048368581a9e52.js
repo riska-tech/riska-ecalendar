0ea2e12ce5c0b3c0f10b3e429a2df921b1ffd1fb
Fariz Hermawan <fariz.hermawan@traveloka.com>
2020-01-25 02:06:57 +0700

    Build for release

---
+++ www/precache-manifest.2a85fedb6601fb3703048368581a9e52.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506c115191852d0b8c3717e662c73522",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80406dbe565312cd6500",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "9ee96e3a07dec6235d3f1a86787a92b7",
+    "url": "fonts/Framework7Icons-Regular.eot"
+  },
+  {
+    "revision": "3b973a05c0e1544f7ee8fa23c2a9315a",
+    "url": "fonts/Framework7Icons-Regular.ttf"
+  },
+  {
+    "revision": "535bcf7dd3feb41ec20507c3c5b81efb",
+    "url": "fonts/Framework7Icons-Regular.woff"
+  },
+  {
+    "revision": "1dfb73e22b9119f547fb880568f56ea2",
+    "url": "fonts/Framework7Icons-Regular.woff2"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "fonts/MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "80406dbe565312cd6500",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "f0f78813f10b0fa6934e0b67cbb196b8",
+    "url": "js/app.js.LICENSE"
+  },
+  {
+    "revision": "43e71940f73e14e21d1cd9d688da7f91",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "0135b8a4b3caf8899e89a587997ab6b9",
+    "url": "static/icons/128x128.png"
+  },
+  {
+    "revision": "1779dd4b0b4ce52e5d98ad71c89caea9",
+    "url": "static/icons/144x144.png"
+  },
+  {
+    "revision": "24af32725c982a6f2ee21a8c74b51b06",
+    "url": "static/icons/152x152.png"
+  },
+  {
+    "revision": "e7fb42afc35f69613b8a1153c855a849",
+    "url": "static/icons/192x192.png"
+  },
+  {
+    "revision": "d014403e73333994eda3cb53f040ba08",
+    "url": "static/icons/256x256.png"
+  },
+  {
+    "revision": "4d7169b9e3529767ee3c112c113f370e",
+    "url": "static/icons/512x512.png"
+  },
+  {
+    "revision": "d014403e73333994eda3cb53f040ba08",
+    "url": "static/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "0135b8a4b3caf8899e89a587997ab6b9",
+    "url": "static/icons/favicon.png"
+  }
+]);
